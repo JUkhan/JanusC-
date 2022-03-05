6bf4dc4399a1073b3250a3e5c1d8caf17ff006a9
JUkhan <jasim.uddin.khan@gmail.com>
2022-03-05 13:05:39 +0600

    init

--- src/JanusApp/lib/plugins/VideoCall.cs
+++ src/JanusApp/lib/plugins/VideoCall.cs
@@ -5,55 +5,56 @@ using System.Threading.Tasks;
 
 namespace JanusApp.lib.plugins
 {
-   
-    public class VideoCall : PluginBase
+
+  public class VideoCall : PluginBase
+  {
+
+    public VideoCall()
     {
-        
-        public VideoCall()
-        {
-            Task.Run(()=>Init());
-        }
-        private async Task Init()
-        {
-            //
-            await Task.Delay(10);
-            OnSuccess();
-        }
-        public void Send(Payload payload) {
-            var message = new Message(payload);
-        }
-        public void Send(Payload payload, object jsep)
-        {
-            var message = new Message(payload, jsep);
-        }
-        public void Data(string text)
-        {
-            
-        }
-        public void Data(string text, Action<Cause> error)
-        {
-
-        }
-        public void Data(string text, Action<Cause> error, Action success)
-        {
-
-        }
-        public void createOffer(bool media, Action<object>success, Action<Cause> error)
-        {
-
-        }
-
-        public void createAnswer(object jsep, Action<object> success, Action<Cause> error)
-        {
-
-        }
-
-        public event Action<object> IceState;
-        public delegate void MessaheHandller(MessageData message, object jsep);
-        public event MessaheHandller OnMessage;
+      Task.Run(() => Init());
+    }
+    private async Task Init()
+    {
+      //Simulating onSuccess
+      await Task.Delay(10);
+      OnSuccess();
+    }
+    public void Send(Payload payload)
+    {
+      var message = new Message(payload);
+    }
+    public void Send(Payload payload, object jsep)
+    {
+      var message = new Message(payload, jsep);
+    }
+    public void Data(string text)
+    {
 
     }
-    public record MessageData(Result result, Cause error);
-    public record Result(List<string> list, string events, string userName);
+    public void Data(string text, Action<Cause> error)
+    {
+
+    }
+    public void Data(string text, Action<Cause> error, Action success)
+    {
+
+    }
+    public void createOffer(bool media, Action<object> success, Action<Cause> error)
+    {
+
+    }
+
+    public void createAnswer(object jsep, Action<object> success, Action<Cause> error)
+    {
+
+    }
+
+    public event Action<object> IceState;
+    public delegate void MessaheHandller(MessageData message, object jsep);
+    public event MessaheHandller OnMessage;
+
+  }
+  public record MessageData(Result result, Cause error);
+  public record Result(List<string> list, string events, string userName);
 
 }
